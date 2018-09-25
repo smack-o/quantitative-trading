bb6cc80cf6ed5469c5ca5956a7665c1b2a68be18
smackgg <smackgg@gmail.com>
2018-09-26 00:56:06 +0800

    delete some file

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import { isMobile } from './uaParser'
-
-const reactChildren = children => (Array.isArray(children) ? children : [children]);
-const loginRouters = ['/signup', '/signin', '/reset']
-export {
-  isMobile,
-  reactChildren,
-  loginRouters
-};
