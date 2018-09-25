9981eebcdba738404fd3d961002bb8f64d40575c
smackgg <smackgg@gmail.com>
2018-09-26 00:55:01 +0800

    add ElementUI; get userinfo befor init app

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+
+const initApp = async () => {
+  await store.dispatch('getUserInfo');
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount('#app');
+};
+
+initApp();
