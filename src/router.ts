3085c1fef3f1e2b5733cd0c338cec3815244be3e
smackgg <smackgg@gmail.com>
2018-09-26 00:55:15 +0800

    udpate router

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,71 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+// import Login from './views/Login.vue';
+const ensure = (name: string) => () => import(`./views/${name}.vue`);
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
+      redirect: { name: 'stgs' },
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/stgs',
+      name: 'stgs',
+      component: ensure('Stgs/index'),
+      children: [{
+        path: '/',
+        name: 'stgsList',
+        component: ensure('Stgs/list'),
+      }, {
+        path: 'help',
+        name: 'help',
+        component: ensure('Stgs/help'),
+      }, {
+        path: 'setting',
+        name: 'stgsSetting',
+        component: ensure('Stgs/setting'),
+      }],
+    },
+    {
+      path: '/user',
+      redirect: { name: 'signin' },
+      children: [{
+        path: 'signin',
+        name: 'signin',
+        component: ensure('Login'),
+      }, {
+        path: 'signup',
+        name: 'signup',
+        component: ensure('Login'),
+      }, {
+        path: 'reset',
+        name: 'reset',
+        component: ensure('Login'),
+      }],
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: ensure('Login'),
     },
   ],
 });
+
+// router.beforeEach((to, from, next) => {
+//   /* must call `next` */
+//   console.log(to)
+//   if (to.name === 'login') {
+//     next();
+//   } else {
+//     console.log(111)
+//     next();
+//   }
+// });
+export default router;
